e3645c6b10175b558c538b38b82e3bd0d4ac4994
sdk-team <sdk-team@alibabacloud.com>
2019-07-19 13:39:35 +0800

    Generated 2015-05-01 for Ram.

--- aliyun-java-sdk-ram/src/main/java/com/aliyuncs/ram/model/v20150501/ClearAccountAliasRequest.java
+++ aliyun-java-sdk-ram/src/main/java/com/aliyuncs/ram/model/v20150501/ClearAccountAliasRequest.java
@@ -16,6 +16,7 @@ package com.aliyuncs.ram.model.v20150501;
 
 import com.aliyuncs.RpcAcsRequest;
 import com.aliyuncs.http.ProtocolType;
+import com.aliyuncs.ram.Endpoint;
 
 /**
  * @author auto create
@@ -26,6 +27,10 @@ public class ClearAccountAliasRequest extends RpcAcsRequest<ClearAccountAliasRes
 	public ClearAccountAliasRequest() {
 		super("Ram", "2015-05-01", "ClearAccountAlias", "ram");
 		setProtocol(ProtocolType.HTTPS);
+		try {
+			this.getClass().getDeclaredField("ProductEndpointMap").set(this, Endpoint.endpointMap);
+			this.getClass().getDeclaredField("ProductEndpointRegional").set(this, Endpoint.endpointRegionalType);
+		} catch (Exception e) {}
 	}
 
 	@Override
