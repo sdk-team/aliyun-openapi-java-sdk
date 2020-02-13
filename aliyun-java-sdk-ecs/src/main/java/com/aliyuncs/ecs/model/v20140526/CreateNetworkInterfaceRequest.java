620eb3239454adba3df5d0397a0953e2e2871008
sdk-team <sdk-team@alibabacloud.com>
2020-02-13 17:34:02 +0800

    Generated 2014-05-26 for Ecs.

---
+++ aliyun-java-sdk-ecs/src/main/java/com/aliyuncs/ecs/model/v20140526/CreateNetworkInterfaceRequest.java
@@ -0,0 +1,288 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ecs.model.v20140526;
+
+import com.aliyuncs.RpcAcsRequest;
+import java.util.List;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class CreateNetworkInterfaceRequest extends RpcAcsRequest<CreateNetworkInterfaceResponse> {
+	   
+
+	private Long resourceOwnerId;
+
+	private String clientToken;
+
+	private String securityGroupId;
+
+	private String description;
+
+	private Integer secondaryPrivateIpAddressCount;
+
+	private String businessType;
+
+	private String resourceGroupId;
+
+	private List<Tag> tags;
+
+	private String networkInterfaceName;
+
+	private Boolean visible;
+
+	private String resourceOwnerAccount;
+
+	private String ownerAccount;
+
+	private Long ownerId;
+
+	private List<String> securityGroupIdss;
+
+	private String vSwitchId;
+
+	private List<String> privateIpAddresss;
+
+	private String primaryIpAddress;
+	public CreateNetworkInterfaceRequest() {
+		super("Ecs", "2014-05-26", "CreateNetworkInterface", "ecs");
+		setMethod(MethodType.POST);
+	}
+
+	public Long getResourceOwnerId() {
+		return this.resourceOwnerId;
+	}
+
+	public void setResourceOwnerId(Long resourceOwnerId) {
+		this.resourceOwnerId = resourceOwnerId;
+		if(resourceOwnerId != null){
+			putQueryParameter("ResourceOwnerId", resourceOwnerId.toString());
+		}
+	}
+
+	public String getClientToken() {
+		return this.clientToken;
+	}
+
+	public void setClientToken(String clientToken) {
+		this.clientToken = clientToken;
+		if(clientToken != null){
+			putQueryParameter("ClientToken", clientToken);
+		}
+	}
+
+	public String getSecurityGroupId() {
+		return this.securityGroupId;
+	}
+
+	public void setSecurityGroupId(String securityGroupId) {
+		this.securityGroupId = securityGroupId;
+		if(securityGroupId != null){
+			putQueryParameter("SecurityGroupId", securityGroupId);
+		}
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+		if(description != null){
+			putQueryParameter("Description", description);
+		}
+	}
+
+	public Integer getSecondaryPrivateIpAddressCount() {
+		return this.secondaryPrivateIpAddressCount;
+	}
+
+	public void setSecondaryPrivateIpAddressCount(Integer secondaryPrivateIpAddressCount) {
+		this.secondaryPrivateIpAddressCount = secondaryPrivateIpAddressCount;
+		if(secondaryPrivateIpAddressCount != null){
+			putQueryParameter("SecondaryPrivateIpAddressCount", secondaryPrivateIpAddressCount.toString());
+		}
+	}
+
+	public String getBusinessType() {
+		return this.businessType;
+	}
+
+	public void setBusinessType(String businessType) {
+		this.businessType = businessType;
+		if(businessType != null){
+			putQueryParameter("BusinessType", businessType);
+		}
+	}
+
+	public String getResourceGroupId() {
+		return this.resourceGroupId;
+	}
+
+	public void setResourceGroupId(String resourceGroupId) {
+		this.resourceGroupId = resourceGroupId;
+		if(resourceGroupId != null){
+			putQueryParameter("ResourceGroupId", resourceGroupId);
+		}
+	}
+
+	public List<Tag> getTags() {
+		return this.tags;
+	}
+
+	public void setTags(List<Tag> tags) {
+		this.tags = tags;	
+		if (tags != null) {
+			for (int depth1 = 0; depth1 < tags.size(); depth1++) {
+				putQueryParameter("Tag." + (depth1 + 1) + ".Key" , tags.get(depth1).getKey());
+				putQueryParameter("Tag." + (depth1 + 1) + ".Value" , tags.get(depth1).getValue());
+			}
+		}	
+	}
+
+	public String getNetworkInterfaceName() {
+		return this.networkInterfaceName;
+	}
+
+	public void setNetworkInterfaceName(String networkInterfaceName) {
+		this.networkInterfaceName = networkInterfaceName;
+		if(networkInterfaceName != null){
+			putQueryParameter("NetworkInterfaceName", networkInterfaceName);
+		}
+	}
+
+	public Boolean getVisible() {
+		return this.visible;
+	}
+
+	public void setVisible(Boolean visible) {
+		this.visible = visible;
+		if(visible != null){
+			putQueryParameter("Visible", visible.toString());
+		}
+	}
+
+	public String getResourceOwnerAccount() {
+		return this.resourceOwnerAccount;
+	}
+
+	public void setResourceOwnerAccount(String resourceOwnerAccount) {
+		this.resourceOwnerAccount = resourceOwnerAccount;
+		if(resourceOwnerAccount != null){
+			putQueryParameter("ResourceOwnerAccount", resourceOwnerAccount);
+		}
+	}
+
+	public String getOwnerAccount() {
+		return this.ownerAccount;
+	}
+
+	public void setOwnerAccount(String ownerAccount) {
+		this.ownerAccount = ownerAccount;
+		if(ownerAccount != null){
+			putQueryParameter("OwnerAccount", ownerAccount);
+		}
+	}
+
+	public Long getOwnerId() {
+		return this.ownerId;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+		if(ownerId != null){
+			putQueryParameter("OwnerId", ownerId.toString());
+		}
+	}
+
+	public List<String> getSecurityGroupIdss() {
+		return this.securityGroupIdss;
+	}
+
+	public void setSecurityGroupIdss(List<String> securityGroupIdss) {
+		this.securityGroupIdss = securityGroupIdss;	
+		if (securityGroupIdss != null) {
+			for (int i = 0; i < securityGroupIdss.size(); i++) {
+				putQueryParameter("SecurityGroupIds." + (i + 1) , securityGroupIdss.get(i));
+			}
+		}	
+	}
+
+	public String getVSwitchId() {
+		return this.vSwitchId;
+	}
+
+	public void setVSwitchId(String vSwitchId) {
+		this.vSwitchId = vSwitchId;
+		if(vSwitchId != null){
+			putQueryParameter("VSwitchId", vSwitchId);
+		}
+	}
+
+	public List<String> getPrivateIpAddresss() {
+		return this.privateIpAddresss;
+	}
+
+	public void setPrivateIpAddresss(List<String> privateIpAddresss) {
+		this.privateIpAddresss = privateIpAddresss;	
+		if (privateIpAddresss != null) {
+			for (int i = 0; i < privateIpAddresss.size(); i++) {
+				putQueryParameter("PrivateIpAddress." + (i + 1) , privateIpAddresss.get(i));
+			}
+		}	
+	}
+
+	public String getPrimaryIpAddress() {
+		return this.primaryIpAddress;
+	}
+
+	public void setPrimaryIpAddress(String primaryIpAddress) {
+		this.primaryIpAddress = primaryIpAddress;
+		if(primaryIpAddress != null){
+			putQueryParameter("PrimaryIpAddress", primaryIpAddress);
+		}
+	}
+
+	public static class Tag {
+
+		private String key;
+
+		private String value;
+
+		public String getKey() {
+			return this.key;
+		}
+
+		public void setKey(String key) {
+			this.key = key;
+		}
+
+		public String getValue() {
+			return this.value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+	}
+
+	@Override
+	public Class<CreateNetworkInterfaceResponse> getResponseClass() {
+		return CreateNetworkInterfaceResponse.class;
+	}
+
+}
