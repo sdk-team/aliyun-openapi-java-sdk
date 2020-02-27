4c5d5a0db67d26a171b4560bc899a1404776ab3e
sdk-team <sdk-team@alibabacloud.com>
2020-02-28 00:39:38 +0800

    Generated 2018-07-13 for Ft.

---
+++ aliyun-java-sdk-ft/src/main/java/com/aliyuncs/ft/model/v20180713/TestErrorCodeForGateWayRequest.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.ft.model.v20180713;
+
+import com.aliyuncs.RpcAcsRequest;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class TestErrorCodeForGateWayRequest extends RpcAcsRequest<TestErrorCodeForGateWayResponse> {
+	   
+
+	private String httpStatusCode;
+
+	private String code;
+
+	private String success;
+
+	private String message;
+	public TestErrorCodeForGateWayRequest() {
+		super("Ft", "2018-07-13", "TestErrorCodeForGateWay");
+		setMethod(MethodType.GET);
+	}
+
+	public String getHttpStatusCode() {
+		return this.httpStatusCode;
+	}
+
+	public void setHttpStatusCode(String httpStatusCode) {
+		this.httpStatusCode = httpStatusCode;
+		if(httpStatusCode != null){
+			putQueryParameter("HttpStatusCode", httpStatusCode);
+		}
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+		if(code != null){
+			putQueryParameter("Code", code);
+		}
+	}
+
+	public String getSuccess() {
+		return this.success;
+	}
+
+	public void setSuccess(String success) {
+		this.success = success;
+		if(success != null){
+			putQueryParameter("Success", success);
+		}
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+		if(message != null){
+			putQueryParameter("Message", message);
+		}
+	}
+
+	@Override
+	public Class<TestErrorCodeForGateWayResponse> getResponseClass() {
+		return TestErrorCodeForGateWayResponse.class;
+	}
+
+}
