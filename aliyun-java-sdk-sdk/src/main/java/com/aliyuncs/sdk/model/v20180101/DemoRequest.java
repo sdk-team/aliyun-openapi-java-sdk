8a1266d6e818db06af6d63792b1a5ccb9cbb53bd
sdk-team <sdk-team@alibabacloud.com>
2020-08-17 07:16:08 +0000

    Generated 2018-01-01 for SDK.

---
+++ aliyun-java-sdk-sdk/src/main/java/com/aliyuncs/sdk/model/v20180101/DemoRequest.java
@@ -0,0 +1,107 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.sdk.model.v20180101;
+
+import com.aliyuncs.RoaAcsRequest;
+import java.util.List;
+import com.aliyuncs.http.MethodType;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DemoRequest extends RoaAcsRequest<DemoResponse> {
+	   
+
+	private String price;
+
+	private String name;
+
+	private List<ContentList> contentLists;
+	public DemoRequest() {
+		super("SDK", "2018-01-01", "Demo");
+		setUriPattern("/helloworld");
+		setMethod(MethodType.POST);
+	}
+
+	public String getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+		if(price != null){
+			putQueryParameter("Price", price);
+		}
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+		if(name != null){
+			putBodyParameter("Name", name);
+		}
+	}
+
+	public List<ContentList> getContentLists() {
+		return this.contentLists;
+	}
+
+	public void setContentLists(List<ContentList> contentLists) {
+		this.contentLists = contentLists;	
+		if (contentLists != null) {
+			for (int depth1 = 0; depth1 < contentLists.size(); depth1++) {
+				putBodyParameter("ContentList." + (depth1 + 1) + ".Tag" , contentLists.get(depth1).getTag());
+				if (contentLists.get(depth1).getLetterss() != null) {
+					for (int i = 0; i < contentLists.get(depth1).getLetterss().size(); i++) {
+						putBodyParameter("ContentList." + (depth1 + 1) + ".Letters." + (i + 1) , contentLists.get(depth1).getLetterss().get(i));
+					}
+				}
+			}
+		}	
+	}
+
+	public static class ContentList {
+
+		private String tag;
+
+		private List<String> letterss;
+
+		public String getTag() {
+			return this.tag;
+		}
+
+		public void setTag(String tag) {
+			this.tag = tag;
+		}
+
+		public List<String> getLetterss() {
+			return this.letterss;
+		}
+
+		public void setLetterss(List<String> letterss) {
+			this.letterss = letterss;
+		}
+	}
+
+	@Override
+	public Class<DemoResponse> getResponseClass() {
+		return DemoResponse.class;
+	}
+
+}
---
+++ aliyun-java-sdk-sdk/src/main/java/com/aliyuncs/sdk/model/v20180101/DemoResponse.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyuncs.sdk.model.v20180101;
+
+import java.util.List;
+import java.util.Map;
+import com.aliyuncs.AcsResponse;
+import com.aliyuncs.sdk.transform.v20180101.DemoResponseUnmarshaller;
+import com.aliyuncs.transform.UnmarshallerContext;
+
+/**
+ * @author auto create
+ * @version 
+ */
+public class DemoResponse extends AcsResponse {
+
+	private String requestId;
+
+	private Map<Object,Object> tetMap;
+
+	private List<Map<Object,Object>> listMap;
+
+	public String getRequestId() {
+		return this.requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	public Map<Object,Object> getTetMap() {
+		return this.tetMap;
+	}
+
+	public void setTetMap(Map<Object,Object> tetMap) {
+		this.tetMap = tetMap;
+	}
+
+	public List<Map<Object,Object>> getListMap() {
+		return this.listMap;
+	}
+
+	public void setListMap(List<Map<Object,Object>> listMap) {
+		this.listMap = listMap;
+	}
+
+	@Override
+	public DemoResponse getInstance(UnmarshallerContext context) {
+		return	DemoResponseUnmarshaller.unmarshall(this, context);
+	}
+
+	@Override
+	public boolean checkShowJsonItemName() {
+		return false;
+	}
+}
